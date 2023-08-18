248c241ecbbf3ff5d094ef3a99e22ba1a41e2a29
Vedant Laddha <122097882+vedantladdha1@users.noreply.github.com>
2023-08-18 16:14:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
- var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+// JavaScript code for the dice rolling game
 
- var randomDiceImage = "dice" + randomNumber1 + ".png" ;
-
-
-var randomImageSource = "images/" + randomDiceImage ;
-
-var img1 = document.querySelectorAll("img")[0];
-
-img1.setAttribute("src" , randomImageSource); 
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
-
-var randomImageSource2 = "images/dice" + randomNumber2 +".png";
-
-document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
-
-if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML ="Player 1 Wins !";
-}
-
-else if(randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "Player 2 Wins !";
-}
-
-else{
-    document.querySelector("h1").innerHTML = "Roll It Once Again";
-}
+// Function to roll the dice and update the images
+function rollDice() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage = "dice" + randomNumber1 + ".png";
+    var randomImageSource = "images/" + randomDiceImage;
+  
+    var img1 = document.querySelectorAll("img")[0];
+    img1.setAttribute("src", randomImageSource);
+  
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+  
+    updateResult(randomNumber1, randomNumber2);
+  }
+  
+  // Function to update the result text
+  function updateResult(number1, number2) {
+    var resultElement = document.querySelector("h1");
+    if (number1 > number2) {
+      resultElement.innerHTML = "Player 1 Wins!";
+    } else if (number2 > number1) {
+      resultElement.innerHTML = "Player 2 Wins!";
+    } else {
+      resultElement.innerHTML = "Roll It Once Again";
+    }
+  }
+  
+  // Get the "Play Again" button element
+  var playAgainButton = document.querySelector(".button");
+  
+  // Add a click event listener to the "Play Again" button
+  playAgainButton.addEventListener("click", rollDice);
+  
+  // Initial roll when the page loads
+  rollDice();
